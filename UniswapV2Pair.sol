bdcc8812a5ff01b42d5a5250cad6046d477d3bdf
Benjam6 <106699745+Benjam6@users.noreply.github.com>
2022-09-28 23:39:41 +0100

    Update and rename MyUniswapV2Pair.sol to UniswapV2Pair.sol

--- UniswapV2Pair.sol
+++ UniswapV2Pair.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.17;
 
-interface MyUniswapV2Pair {
+interface UniswapV2Pair {
     event Approval(
         address indexed owner,
         address indexed spender,
