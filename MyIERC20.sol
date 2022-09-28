28c62678f28421cb36055a73d9e701524e364be5
Benjam6 <106699745+Benjam6@users.noreply.github.com>
2022-09-28 23:25:28 +0100

    Update MyIERC20.sol

--- MyIERC20.sol
+++ MyIERC20.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.17;
 
-interface MyIERC20 {
+interface IERC20 {
     event Approval(
         address indexed owner,
         address indexed spender,
