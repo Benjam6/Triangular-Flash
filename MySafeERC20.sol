4bba9772a2a32184f785d835d85ca80d310cc40a
Benjam6 <106699745+Benjam6@users.noreply.github.com>
2022-09-28 22:50:42 +0100

    Update MySafeERC20.sol

--- MySafeERC20.sol
+++ MySafeERC20.sol
@@ -7,7 +7,7 @@ import "https://github.com/Benjam6/Triangular-Flash/blob/main/MyAddress.sol";
 
 /**
  * @title MYSafeERC20
- * @dev Wrappers around ERC20 operations that throw on failure (when the token
+ * @dev  the Wrappers around ERC20 operations that throw on failure (when the token
  * contract returns false). Tokens that return no value (and instead revert or
  * throw on failure) are also supported, non-reverting calls are assumed to be
  * successful.
